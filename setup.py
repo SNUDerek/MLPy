d86872fa49e9f14ff1be2927ceecc8341ffc355c
SNUDerek <dsh9470@snu.ac.kr>
2018-03-26 17:49:40 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(name='mlpy',
+      version='0.1.0',
+      description='an educational python-based ML library',
+      url='https://github.com/SNUDerek/MLPy',
+      author='Derek Hommel',
+      author_email='dsh9470@snu.ac.kr',
+      # license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
